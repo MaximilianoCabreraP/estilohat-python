df50e1dd6011e72e7a15b11d5d1fe78b5d9178ac
MaximilianoCabreraP <maximilianocabrerap@gmail.com>
2019-02-18 21:43:29 -0300

    edit pedido v0.10

--- application/models/Pedido_model.php
+++ application/models/Pedido_model.php
@@ -14,6 +14,11 @@ class Pedido_model extends CI_Model{
         return $this->db->affected_rows()>0;
     }
     public function editar_pedido($pedido){
+        $this->db->select_max('version');
+        $version = $this->db->get_where('pedido', ['id_pedido' => $pedido])->row_array();
+        log_message('error', 'Version: '.$version);
+        $pedido['version'] = $version+1;
+        $pedido['nombre_modificacion'] = 'Meme';
         $this->db->where('id_pedido', $pedido['id_pedido'])
                  ->update('pedido', $pedido);
         return $this->db->affected_rows()>0;
