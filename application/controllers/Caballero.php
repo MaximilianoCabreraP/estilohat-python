ddfb2eb321ea30b6576b160b8e0083575bc0afc4
Maximiliano Cabrera <39493720+MaximilianoCabreraP@users.noreply.github.com>
2020-03-23 13:50:07 -0300

    sskotz v0.13

--- application/controllers/Caballero.php
+++ application/controllers/Caballero.php
@@ -3,10 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Caballero extends CI_Controller{
 	public function index($msg=""){
         log_message("error", "-----Listado Caballero-----");
-        $this->load->view("sskotz/header");
+        $this->load->view("sskotz/header", ["title" => "Listado Caballeros"]);
         $this->load->view("sskotz/listado_caballeros", ["caballeros" => $this->Caballero_model->buscar_caballeros(), "msg" => $msg]);
         $this->load->view("sskotz/footer");
-	}/*
+	}
 	public function nuevo($msg=""){
         log_message("error", "-----Nuevo Caballero-----");
         $this->load->view("sskotz/header");
@@ -22,5 +22,5 @@ class Caballero extends CI_Controller{
         );
         log_message("error", "Data: ".print_r($data, true));
         $this->load->view("json", ["data" => $data]);
-    }*/
+    }
 }
