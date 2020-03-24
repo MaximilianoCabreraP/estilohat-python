70dd76808c43c05fe0c938fd6df1242757526f52
Maximiliano Cabrera <39493720+MaximilianoCabreraP@users.noreply.github.com>
2020-03-24 16:35:55 -0300

    sskotz v0.20
    Dejo para despues

--- application/config/database.php
+++ application/config/database.php
@@ -26,27 +26,6 @@ $db['default'] = array(
 	'save_queries' => FALSE
 );
 
-$db['sskotz'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'estilohat',
-	'password' => 'V87txSzuyLDzcfH8NGx8YL9T',
-	'database' => 'saintseiyakotz',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => FALSE
-);
 // $db['sskotz'] = array(
 // 	'dsn'	=> '',
 // 	'hostname' => 'localhost',
