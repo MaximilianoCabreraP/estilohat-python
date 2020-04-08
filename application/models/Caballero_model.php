8c112cc626612d9a93e97908f8cdee44ac3116c2
Maximiliano Cabrera <39493720+MaximilianoCabreraP@users.noreply.github.com>
2020-04-08 18:53:29 -0300

    fix db v0.2

--- application/models/Caballero_model.php
+++ application/models/Caballero_model.php
@@ -1,14 +1,14 @@
 <?php
     class Caballero_model extends CI_Model{
         function __construct(){
-            //$this->dbss = $this->load->database("sskotz",TRUE);
+            $this->sskotz = $this->load->database("sskotz",TRUE);
 
             date_default_timezone_set("America/Argentina/Buenos_Aires");
         }
         public function buscar_caballeros(){
-            return $this->dbss->get("caballeros")->result_array();
+            return $this->sskotz->get("caballeros")->result_array();
         }
         public function buscar_caballero($id){
-            return $this->dbss->get_where("caballeros", ["id"=>$id])->row_array();
+            return $this->sskotz->get_where("caballeros", ["id"=>$id])->row_array();
         }
     }
