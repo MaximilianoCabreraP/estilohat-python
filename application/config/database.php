b597243421c1d250439f95e76234ffb6f033107d
Maximiliano Cabrera <39493720+MaximilianoCabreraP@users.noreply.github.com>
2020-04-08 18:44:14 -0300

    fix db v0.1

--- application/config/database.php
+++ application/config/database.php
@@ -26,24 +26,24 @@ $db['default'] = array(
 	'save_queries' => FALSE
 );
 
-// $db['sskotz'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => 'localhost',
-// 	'username' => 'meme',
-// 	'password' => 'wQa3cdL4SMqZaCC7aXFGFLmu5G',
-// 	'database' => 'saintseiyakotz',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => FALSE
-// );
+$db['sskotz'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'saintseiyakotz',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => FALSE
+);
