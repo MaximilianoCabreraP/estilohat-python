5d6169d808ce7161309ed6d2fe71a14caf495639
Maximiliano Cabrera <39493720+MaximilianoCabreraP@users.noreply.github.com>
2020-04-09 08:59:34 -0300

    View Caballero v0.4

--- application/controllers/Caballero.php
+++ application/controllers/Caballero.php
@@ -2,21 +2,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Caballero extends CI_Controller{
 	public function index($msg=""){
-        log_message("error", "-----Listado Caballero-----");
         $caballeros = $this->Caballero_model->buscar_caballeros();
-        log_message("error", "Caballeros: ".print_r($caballeros, true));
         $this->load->view("sskotz/header", ["title" => "Listado Caballeros"]);
         $this->load->view("sskotz/listado_caballeros", ["caballeros" => $caballeros, "msg" => $msg, "msj" => $this->session->flashdata("msj")]);
         $this->load->view("sskotz/footer");
 	}
 	public function nuevo($msg=""){
-        log_message("error", "-----Nuevo Caballero-----");
         $this->load->view("sskotz/header", ["title" => "Nuevo Caballero"]);
         $this->load->view("sskotz/ingresar_caballero", ["caballero" => $this->session->flashdata("caballero")]);
         $this->load->view("sskotz/footer");
     }
     public function agregar_caballero(){
-        log_message("error", "------------Agregar Caballero------------");
         $caballero = $this->input->post();
         if($this->Caballero_model->crear_caballero($caballero)){
             $this->session->set_flashdata("msj", "Se creó la ficha de $caballero[nombre] correctamente.");
@@ -28,9 +24,11 @@ class Caballero extends CI_Controller{
     }
 
     public function saint($nombre){
+        log_message("error", "Nombre: $caballero");
         $caballero = $this->Caballero_model->buscar_caballero_by_nombre($nombre);
+        log_message("error", "Caballero: ".print_r($caballero, true));
         $this->load->view("sskotz/header", ["title" => "Caballero "]);
-        $this->load->view("sskotz/caballero", ["caballero" => $caballero]);
+        $this->load->view("sskotz/saint", ["caballero" => $caballero]);
         $this->load->view("sskotz/footer");
     }
 }
