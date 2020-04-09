9ce270b593f62d798d35a9b689e046eeab6491b5
Maximiliano Cabrera <39493720+MaximilianoCabreraP@users.noreply.github.com>
2020-04-09 08:34:15 -0300

    fix db v0.15

--- application/models/Caballero_model.php
+++ application/models/Caballero_model.php
@@ -7,9 +7,13 @@
         }
         public function buscar_caballeros(){
             log_message("error", "BUsco Caballeros");
-            return $this->sskotz->get("saintseiyakotz.caballeros")->result_array();
+            return $this->sskotz->get("caballeros")->result_array();
         }
         public function buscar_caballero($id){
             return $this->sskotz->get_where("caballeros", ["id"=>$id])->row_array();
+        }
+        public function crear_caballero($caballero){
+            $this->sskotz->insert("caballeros", $caballero);
+            return $this->sskotz->affected_rows() > 0;
         }
     }
